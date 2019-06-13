282fbbd0f12e2b7eb48ff9366feb84ee585cdb96
Jennifer Menze <jamenze@gmail.com>
2019-06-13 06:32:27 -0400

    JS updates

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(1920, 1080, Phaser.Auto)
+
+var GameState = {
+  preload: function() {
+
+  },
+
+  create: function() {
+
+  },
+  
+  update: function() {
+
+  }
+}
+
+game.state.add('GameState', GameState)
+game.state.start('GameState')
